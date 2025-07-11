d2964efa491021347414ec2d97b9e91bcd5c2891
MarianaMH1195 <morenomariana03@gmail.com>
2025-07-11 14:38:15 +0200

    finish project

--- src/services.js
+++ src/services.js
@@ -1,20 +1,21 @@
-let editingBookId = null;
+// minimaza errores 
+let editingMovieId = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-    printBook();
+    printMovie();
 });
 
 // METODO GET funcion para cargar la speliculas de la base de datos
 async function getAllmovies() {
-    const response = await fetch("http://localhost:3000/movies",{
+    const response = await fetch("http://localhost:3000/movies", {
         method: "GET",
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    
-    
-    
+
+
+
     const movieData = await response.json()
     console.log(movieData)
     return movieData
@@ -32,9 +33,9 @@ async function printMovie(params) {
        <p>${movie.director}</p>
        <p>${movie.year}</p>
        <p>${movie.country}</p>
-       <p>${movie.gender}</p>
-
-        <button onclick="deleteMovie('${movie.id}')">Elimiar</button>
+       <p>${movie.genre}</p>
+       <p>${movie.synopsis}</p>
+        <button onclick="deleteMovie('${movie.id}')">Eliminar</button>
         <button onclick="getMovieById('${movie.id}')">Editar</button> `
     });
     return movieList
@@ -59,5 +60,118 @@ async function deleteMovie(id) {
 async function addMovie(event) {
     event.preventDefault();
     const title = document.getElementById("title").value;
-    const director = document.getElementById("ditector").value;
+    const director = document.getElementById("director").value;
     const year = document.getElementById("year").value;
+    const country = document.getElementById("country").value;
+    const genre = document.getElementById("genre").value;
+    const synopsis = document.getElementById("synopsis").value;
+
+    const movienew = {
+        title,
+        director,
+        year,
+        country,
+        genre,
+        synopsis,
+
+    };
+
+    const response = await fetch("http://localhost:3000/movies", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movienew)
+    });
+
+    console.log("Código de estado", response.status)
+    if (response.ok) {
+        const data = await response.json()
+        console.log("pelicula creada", data)
+        await printMovie();
+    } else {
+        const error = await response.text()
+        console.log('error al cargar pelicula', error);
+    }
+
+
+}
+
+// Escuchar el envío del formulario
+const form = document.getElementById("movieForm");
+form.addEventListener("submit", addMovie);
+
+//Método put
+async function getMovieById(id) {
+
+    const response = await fetch(`http://localhost:3000/movies/${id}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+
+          const movie = await response.json();
+
+        const title = document.getElementById("title").value = movie.title;
+        const director = document.getElementById("director").value = movie.director;
+        const year = document.getElementById("year").value = movie.year;
+        const country = document.getElementById("country").value = movie.country;
+        const genre = document.getElementById("genre").value = movie.genre;
+        const synopsis = document.getElementById("synopsis"). value = movie.synopsis;
+        editingMovieId = movie.id
+
+           }else{
+        console.log("error")
+    }
+
+}
+
+async function updateMovie(event){
+    event.preventDefault()
+
+     if(!editingMovieId){
+        console.log("no hay pelicula selecciona para actualizar")
+        return;
+    }
+
+        const title = document.getElementById("title").value;
+        const director = document.getElementById("director").value ;
+        const year = document.getElementById("year").value;
+        const country = document.getElementById("country").value;
+        const genre = document.getElementById("genre").value;
+        const synopsis = document.getElementById("synopsis"). value;
+
+        const updateMovie = {
+        title,
+        director,
+        year,
+        country,
+        genre,
+        synopsis,
+
+    };
+
+       const response = await fetch(`http://localhost:3000/movies/${editingMovieId}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(updateMovie)
+    });
+
+    if(response.ok){
+
+        console.log("Pelicula bien actualizada")
+        document.getElementById("movieForm").reset()
+        editingMovieId = null
+        await printMovie()
+    }else{
+        console.log("error")
+    }
+}
+
+const updateButton = document.getElementById("updateButton")
+updateButton.addEventListener('click', updateMovie)
